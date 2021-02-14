47fbc53dea5149b11f36f82a66eda44b7dd71fef
Freddy Gonzalez <fredo_y@hotmail.com>
2021-02-13 21:41:47 -0600

    fist commit

---
+++ app/Http/Controllers/ComplaintControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ComplaintController;
+use Illuminate\Http\Request;
+
+class ComplaintControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ComplaintController  $complaintController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ComplaintController $complaintController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ComplaintController  $complaintController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ComplaintController $complaintController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ComplaintController  $complaintController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ComplaintController $complaintController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ComplaintController  $complaintController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ComplaintController $complaintController)
+    {
+        //
+    }
+}
